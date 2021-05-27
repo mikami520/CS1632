42c586a42c1b336efcbf52757c65e9cde24e1bd3
“SaKuRaXiAo0603” <yux37@pitt.edu>
2021-05-27 18:41:25 -0400

    Update

---
+++ Exercise0/SortedCollection.java
@@ -0,0 +1,80 @@
+/*
+ * @Author: Yuliang Xiao 
+ * @Date: 2021-05-19 00:22:50 
+ * @Last Modified by:   Yuliang Xiao 
+ * @Last Modified time: 2021-05-19 00:22:50 
+ */
+//TODO: Import libraries as needed
+import java.util.*;
+import java.util.NoSuchElementException;
+
+public class SortedCollection {
+	// TODO: Add member variables or methods as needed
+	List<Integer> list = new ArrayList<>();
+
+	/**
+	 * Adds the number n to the collection.
+	 * 
+	 * @param n the number to add to the collection
+	 * @return always returns true
+	 */
+	public boolean add(int n) {
+		// TODO: Implement
+		list.add(n);
+		return true;
+	}
+
+	/**
+	 * Removes the smallest number in the collection and returns it.
+	 * If the collection is empty, throws a NoSuchElementException.
+	 * 
+	 * @return the smallest number in the collection
+	 */
+	public int remove() throws NoSuchElementException {
+		// TODO: Implement
+		if(list.isEmpty()) {
+			throw new NoSuchElementException();
+		}
+		Collections.sort(list);
+		int result = list.remove(0);
+		return result;
+	}
+
+	/**
+	 * Prints usage information.
+	 */
+	public static void showUsage() {
+		System.out.println("Usage: java SortedCollection [num1] [num2] [num3] ...");
+	}
+
+	/**
+	 * Main method. Receives a list of numbers as commandline arguments and prints
+	 * out the list in sorted order from smallest to largest.
+	 * 
+	 * @param args commandline arguments; see showUsage() for detailed information
+	 */
+	public static void main(String[] args) {
+		SortedCollection collection = new SortedCollection();
+		if (args.length == 0) {
+			showUsage();
+			return;
+		}
+		// TODO: add numbers in commandline arguments to collection using the add(int) method.
+		// If any commandline argument is not a number, call showUsage() and return.
+		for(int i = 0; i < args.length; i++) {
+			try {
+				int ele = Integer.parseInt(args[i]);
+				collection.add(ele);
+			} catch (NumberFormatException e) {
+				showUsage();
+				return;
+			}
+		}
+		System.out.print("sorted: ");
+		for (int i = 0; i < args.length; i++) {
+			int num = collection.remove();
+			System.out.print(num + " ");
+		}
+		System.out.println();
+	}
+}
